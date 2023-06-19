b673b84758a8c9a386b69823a0b70abdf9eb534d
tsungkaii <130960098+tsungkaii@users.noreply.github.com>
2023-06-19 14:05:26 +0800

    Add files via upload

---
+++ 111/README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/tsungkai/pen/LYgoPeb](https://codepen.io/tsungkai/pen/LYgoPeb).
+
---
+++ 111/src/script.js
@@ -0,0 +1,8 @@
+
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
